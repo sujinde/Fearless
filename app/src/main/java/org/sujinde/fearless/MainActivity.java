34f0374c436ba1bbc2136b5c82220cb13883a73a
Mr.SuJinde <sujinde168@foxmail.com>
2016-06-02 18:17:09 +0800

    备忘想用的库

--- app/src/main/java/org/sujinde/fearless/MainActivity.java
+++ app/src/main/java/org/sujinde/fearless/MainActivity.java
@@ -28,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
 		});
 	}
 
+	void scala()
+	{
+		
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
